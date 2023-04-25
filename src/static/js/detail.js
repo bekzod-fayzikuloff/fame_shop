7acaaacf2d7b376256df5de9d48a067a0473b6be
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-04-26 01:24:43 +0500

    :hammer: Add cart base bl

---
+++ src/static/js/detail.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setupListeners()
+});
+
+function setupListeners() {
+    /*
+    * Handling item count increment and decrement
+    * */
+    const decrementBtn = document.querySelector("#decrementItem")
+    const incrementBtn = document.querySelector("#incrementItem")
+
+    incrementBtn.addEventListener("click", () => {
+        let quantity = document.querySelector("#quantity")
+        if (isNaN(+quantity.value)) {
+            quantity.value = 1
+        } else {
+            quantity.value = +quantity.value + 1
+        }
+    })
+
+    decrementBtn.addEventListener("click", () => {
+        let quantity = document.querySelector("#quantity")
+        if (isNaN(+quantity.value) || +quantity.value <= 1) {
+            quantity.value = 1
+        } else {
+            quantity.value = +quantity.value - 1
+        }
+    })
+
+    document.querySelector("#addToCart").addEventListener("click", () => {
+        /*
+        * Cart items add handling
+        * Add Items and items quantity to localStorage
+        * */
+        let productId = document.querySelector("#productId").innerHTML
+        let quantity = document.querySelector("#quantity").value
+
+        if (localStorage.getItem("cart")) {
+            let cart = JSON.parse(localStorage.getItem("cart"))
+            if (!cart.filter(item => item.productId === productId).length) {
+                cart = [{productId, quantity: +quantity}, ...cart]
+            } else {
+                let index = cart.findIndex(item => item.productId === productId)
+                if (index !== -1) {
+                    cart[index].quantity += +quantity
+                }
+            }
+            localStorage.removeItem("cart")
+            localStorage.setItem("cart", JSON.stringify(cart))
+        } else {
+            localStorage.setItem("cart", JSON.stringify([{productId, quantity: +quantity}]))
+        }
+        alert("Товар был добавлен в корзину")
+    })
+}
